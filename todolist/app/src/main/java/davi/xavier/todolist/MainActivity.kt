7f45aaee92d5591d5a56e3739aaea1a911031217
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2021-05-22 18:46:04 -0300

    [projects/todolist] - Initialize todolist folder

---
+++ todolist/app/src/main/java/davi/xavier/todolist/MainActivity.kt
@@ -0,0 +1,21 @@
+package davi.xavier.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import davi.xavier.todolist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        binding.addButton.setOnClickListener { add() }
+    }
+    
+    fun add() {
+        
+    }
+}
